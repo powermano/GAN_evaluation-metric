236279b57dbe46e4eea830a5700d744bb85b5cfb
ct <894773140@qq.com>
2019-04-14 17:26:09 +0800

    add training codes

---
+++ resnet-socre/for_test.py
@@ -0,0 +1,28 @@
+import numpy as np
+import tensorflow as tf
+
+
+def next_batch():
+    datasets = np.asarray(range(0, 20))
+    input_queue = tf.train.slice_input_producer([datasets], shuffle=False)
+    data_batchs = tf.train.batch(input_queue, batch_size=6, num_threads=1,
+                                 capacity=18, allow_smaller_final_batch=False)
+    return data_batchs
+
+
+if __name__ == "__main__":
+    data_batchs = next_batch()
+    sess = tf.Session()
+    sess.run(tf.local_variables_initializer())
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess, coord)
+    try:
+        while not coord.should_stop():
+            data = sess.run([data_batchs])
+            print(data)
+    except tf.errors.OutOfRangeError:
+        print("complete")
+    finally:
+        coord.request_stop()
+    coord.join(threads)
+    sess.close()
--- resnet-socre/model.py
+++ resnet-socre/model.py
@@ -18,6 +18,11 @@ from tensorflow.contrib.layers.python.layers import regularizers, \
 # import cv2
 from PIL import Image
 import os
+from input_train import *
+
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
 
 def resnet_arg_scope(
         is_training=True, weight_decay=0.0001, batch_norm_decay=0.997,
@@ -44,15 +49,18 @@ def resnet_arg_scope(
 is_training = True
 global_pool = True
 spatial_squeeze = True
-num_classes = 2
+num_classes = 3
 depth_multiplier = 1
 min_base_depth = 8
 batch_size = 1  # test_batch
+BATCH = 32  # train_batch size
 img_height = 224
 img_width = 224
 img_layer = 3
 img_size = img_height * img_width
-image = tf.placeholder(tf.float32, shape=[1, 224, 224, 3])
+learning_rate = 0.0001   # with current parameters, it is suggested to use learning rate<0.0001
+image = tf.placeholder(tf.float32, shape=[BATCH, 224, 224, 3])
+y = tf.placeholder(tf.int32, shape=[BATCH])
 
 def model():
     depth_func = lambda d: max(int(d * depth_multiplier), min_base_depth)
@@ -102,7 +110,7 @@ def model():
 
         if num_classes:
             net_final = slim.conv2d(net_global, num_classes, [1, 1], activation_fn=None,
-                                    normalizer_fn=None, scope='logits')
+                                    normalizer_fn=None, scope='logits_fixed')   # default is logits, change it for train sonar images
             if spatial_squeeze:
                 net_squzee = tf.squeeze(net_final, [1, 2], name='SpatialSqueeze')
             cls_pred = tf.arg_max(net_squzee, 1)
@@ -194,15 +202,138 @@ def get_inception_socres(predictions, splits=10):
     return np.mean(scores), np.std(scores)
 
 
+def losses(logits, labels):
+    '''Compute loss from logits and labels
+    Args:
+        logits: logits tensor, float, [batch_size, n_classes]
+        labels: label tensor, tf.int32, [batch_size]
+
+    Returns:
+        loss tensor of float type
+    '''
+    with tf.variable_scope('loss') as scope:
+        cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits \
+            (logits=logits, labels=labels, name='xentropy_per_example')
+        loss = tf.reduce_mean(cross_entropy, name='loss')
+        tf.summary.scalar(scope.name + '/loss', loss)
+    return loss
+
+
+def trainning(loss, learning_rate):
+    '''Training ops, the Op returned by this function is what must be passed to
+        'sess.run()' call to cause the model to train.
+
+    Args:
+        loss: loss tensor, from losses()
+
+    Returns:
+        train_op: The op for trainning
+    '''
+    with tf.name_scope('optimizer'):
+        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+        global_step = tf.Variable(0, name='global_step', trainable=False)
+        train_op = optimizer.minimize(loss, global_step=global_step)
+    return train_op
+
+def evaluation(logits, labels):
+  """Evaluate the quality of the logits at predicting the label.
+  Args:
+    logits: Logits tensor, float - [batch_size, NUM_CLASSES].
+    labels: Labels tensor, int32 - [batch_size], with values in the
+      range [0, NUM_CLASSES).
+  Returns:
+    A scalar int32 tensor with the number of examples (out of batch_size)
+    that were predicted correctly.
+  """
+  with tf.variable_scope('accuracy') as scope:
+      correct = tf.nn.in_top_k(logits, labels, 1)
+      correct = tf.cast(correct, tf.float16)
+      accuracy = tf.reduce_mean(correct)
+      tf.summary.scalar(scope.name+'/accuracy', accuracy)
+  return accuracy
+
 def train(image_dir):
+    cls, softmax_result = model()
+    # id_list = os.listdir(image_dir)
+    # image_list = []
+    # label_list = []
+    # for id in id_list:
+    #     image_list.append(os.path.join(image_dir, id))
+    #     label_list.append(1)
+    # label_list = [int(float(x)) for x in label_list]
+    image_list, label_list, val_image_list, val_image_label = get_files(image_dir,
+                    './trainval_list/trainval.txt', './trainval_list/test.txt')
+    train_batch, label_batch = get_batch(image_list, label_list, img_width, img_height, BATCH, 5000)
+    val_batch, val_label_batch = get_batch(val_image_list, val_image_label, img_width, img_height, BATCH, 5000)
+    loss = losses(softmax_result, label_batch)
+    train_op = trainning(loss, learning_rate)
+    acc = evaluation(softmax_result, label_batch)
+    with tf.Session() as sess:
+        restore_list = [x for x in tf.trainable_variables() if 'logit' not in x.name]
+        saver = tf.train.Saver(restore_list)
+        init = [tf.global_variables_initializer(), tf.local_variables_initializer()]
+        sess.run(init)
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(coord=coord)
+        #chkpt_fname = r'E:\resnet_v1_50.ckpt'
+        chkpt_fname = '/home/ct/resnet_v1_50.ckpt'
+        print(chkpt_fname)
+        print('loading model ...............')
+        saver.restore(sess, chkpt_fname)
+        print('Suessfully load the model................')
+        #logs_train_dir = r'E:\detection paper\train_result_dir'
+        logs_train_dir = './logs/train'
+        logs_val_dir = './logs/val'
+        summary_op = tf.summary.merge_all()
+        train_writer = tf.summary.FileWriter(logs_train_dir, sess.graph)
+        val_writer = tf.summary.FileWriter(logs_val_dir, sess.graph)
+        MAX_STEP = 5000
+        try:
+            for step in np.arange(MAX_STEP):
+                if coord.should_stop():
+                    break
+
+                tra_images, tra_labels = sess.run([train_batch, label_batch])
+                print(tra_images.shape)
+                print(tra_labels.shape)
+                _, tra_loss, tra_acc = sess.run([train_op, loss, acc],
+                                                feed_dict={image: tra_images, y: tra_labels})
+                if step % 20 == 0:
+                    print('Step %d, train loss = %.2f, train accuracy = %.2f%%' % (step, tra_loss, tra_acc * 100.0))
+                    summary_str = sess.run(summary_op, feed_dict={image: tra_images, y: tra_labels})
+                    train_writer.add_summary(summary_str, step)
+
+                if step % 40 == 0 or (step + 1) == MAX_STEP:
+                    val_images, val_labels = sess.run([val_batch, val_label_batch])
+                    val_loss, val_acc = sess.run([loss, acc],
+                                                 feed_dict={image: val_images, y: val_labels})
+                    print('**  Step %d, val loss = %.2f, val accuracy = %.2f%%  **' % (step, val_loss, val_acc * 100.0))
+                    summary_str = sess.run(summary_op, feed_dict={image: val_images, y: val_labels})
+                    val_writer.add_summary(summary_str, step)
+
+                if step % 5000 == 0 or (step + 1) == MAX_STEP:
+                    checkpoint_path = os.path.join(logs_train_dir, 'sonar.ckpt')
+                    saver.save(sess, checkpoint_path, global_step=step)
+
+        except tf.errors.OutOfRangeError:
+            print('Done training -- epoch limit reached')
+        finally:
+            coord.request_stop()
+        coord.join(threads)
+
     
     
-    
-# image_path = r'E:\cat.jpg'
+#image_path = r'E:\cat.jpg'
 # a = test_per_image(image_path)
 
+# ** calculate inception scores
 #image_dir = '/home/ct/CycleGAN/input/wreck/trainB'
-image_dir = r'E:\inception_scores\patchGAN_16'
-cls_pred, softmax_out = test_images(image_dir)
-mean_socres, std_scores = get_inception_socres(softmax_out)
-print(mean_socres, std_scores)
+# image_dir = r'E:\inception_scores\patchGAN_16'
+# cls_pred, softmax_out = test_images(image_dir)
+# mean_socres, std_scores = get_inception_socres(softmax_out)
+# print(mean_socres, std_scores)
+
+# train sonar classification using pre-trainded model on Imagenet with resnetv1_50
+if __name__== '__main__':
+    image_dir = './images/JPEGImages'
+    train(image_dir)
